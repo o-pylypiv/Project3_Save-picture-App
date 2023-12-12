15339e320f36d01e870ddbd2e87110f37ce81f85
Olha Pylypiv <bassenia@gmail.com>
2023-12-12 20:20:33 +0200

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Olha Pylypiv on 12.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
